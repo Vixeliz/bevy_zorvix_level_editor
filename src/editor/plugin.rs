30383c8a54887dd701856e80797422fecc95c490
Mylah Dee <vixeliz@protonmail.com>
2023-05-26 23:08:53 -0500

    Dark

--- src/editor/plugin.rs
+++ src/editor/plugin.rs
@@ -13,6 +13,21 @@ pub struct EditingLevel(pub Level);
 
 pub struct EditorPlugin;
 
+#[derive(Default, PartialEq, Eq)]
+pub enum EditMode {
+    #[default]
+    Move,
+    AddWall,
+    Delete,
+    AddObject,
+}
+
+#[derive(Resource, Default)]
+pub struct EditorState {
+    pub level_name: String,
+    pub mode: EditMode,
+}
+
 #[derive(Resource, Default, Deref, DerefMut)]
 pub struct EditorPass(pub RenderLayers);
 
@@ -26,6 +41,7 @@ impl Plugin for EditorPlugin {
         app.add_plugins(DefaultPickingPlugins);
         app.insert_resource(EditingLevel::default());
         app.insert_resource(EditorImage::default());
+        app.insert_resource(EditorState::default());
         app.insert_resource(EditorPass(RenderLayers::layer(1)));
         app.add_startup_system(setup);
         app.add_system(render_to_image);
--- src/editor/ui.rs
+++ src/editor/ui.rs
@@ -3,9 +3,6 @@ use bevy_infinite_grid::InfiniteGridBundle;
 use bevy_mod_picking::prelude::RaycastPickCamera;
 use bevy_mod_picking::prelude::RaycastPickTarget;
 use bevy_mod_picking::PickableBundle;
-use catppuccin::Flavour;
-use catppuccin_egui::set_theme;
-use catppuccin_egui::MOCHA;
 use std::f32::consts::TAU;
 
 use bevy::{core_pipeline::clear_color::ClearColorConfig, prelude::*};
@@ -13,6 +10,8 @@ use bevy_egui::*;
 
 use super::camera::PanOrbitCamera;
 use super::camera_egui::EguiWantsFocus;
+use super::plugin::EditMode;
+use super::plugin::EditorState;
 
 pub fn setup(
     mut commands: Commands,
@@ -20,21 +19,20 @@ pub fn setup(
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
-    set_theme(contexts.ctx_mut(), MOCHA);
-    commands
-        .spawn((PbrBundle {
-            mesh: meshes.add(shape::Cube::default().into()),
-            material: materials.add(StandardMaterial {
-                base_color: Color::hex(Flavour::Mocha.mauve().hex()).unwrap(),
-                ..default()
-            }),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
-            ..default()
-        },))
-        .insert((
-            PickableBundle::default(), // Makes the entity pickable
-            RaycastPickTarget::default(),
-        ));
+    // commands
+    //     .spawn((PbrBundle {
+    //         mesh: meshes.add(shape::Cube::default().into()),
+    //         material: materials.add(StandardMaterial {
+    //             base_color: Color::hex(Flavour::Mocha.mauve().hex()).unwrap(),
+    //             ..default()
+    //         }),
+    //         transform: Transform::from_xyz(0.0, 0.5, 0.0),
+    //         ..default()
+    //     },))
+    //     .insert((
+    //         PickableBundle::default(), // Makes the entity pickable
+    //         RaycastPickTarget::default(),
+    //     ));
 
     commands.spawn(PointLightBundle {
         transform: Transform::from_xyz(0.0, 10.0, 10.0),
@@ -44,9 +42,7 @@ pub fn setup(
     commands
         .spawn(Camera3dBundle {
             camera_3d: Camera3d {
-                clear_color: ClearColorConfig::Custom(
-                    Color::hex(Flavour::Mocha.mantle().hex()).unwrap(),
-                ),
+                clear_color: ClearColorConfig::Custom(Color::rgb_u8(0, 0, 0)),
                 ..default()
             },
             ..default()
@@ -60,15 +56,19 @@ pub fn setup(
 
     commands.spawn(InfiniteGridBundle {
         grid: InfiniteGrid {
-            x_axis_color: Color::hex(Flavour::Mocha.red().hex()).unwrap(),
-            z_axis_color: Color::hex(Flavour::Mocha.blue().hex()).unwrap(),
+            x_axis_color: Color::rgb_u8(100, 100, 100),
+            z_axis_color: Color::rgb_u8(200, 200, 200),
             ..Default::default()
         },
         ..Default::default()
     });
 }
 
-pub fn render_to_image(mut contexts: EguiContexts, mut wants_focus: ResMut<EguiWantsFocus>) {
+pub fn render_to_image(
+    mut contexts: EguiContexts,
+    mut wants_focus: ResMut<EguiWantsFocus>,
+    mut editor_state: ResMut<EditorState>,
+) {
     let ctx = contexts.ctx_mut();
     let hover_one = egui::SidePanel::left("Properties")
         .default_width(250.0)
@@ -81,8 +81,17 @@ pub fn render_to_image(mut contexts: EguiContexts, mut wants_focus: ResMut<EguiW
 
                 ui.horizontal(|ui| {
                     ui.label("Level Name: ");
-                    ui.text_edit_singleline(&mut "Name");
+                    ui.text_edit_singleline(&mut editor_state.level_name);
                 });
+
+                ui.separator();
+
+                ui.radio_value(&mut editor_state.mode, EditMode::Move, "Move");
+                ui.radio_value(&mut editor_state.mode, EditMode::AddWall, "AddWall");
+                ui.radio_value(&mut editor_state.mode, EditMode::Delete, "Delete");
+                ui.radio_value(&mut editor_state.mode, EditMode::AddObject, "AddObject");
+
+                ui.separator();
             });
         })
         .response
